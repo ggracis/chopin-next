1744cd74dc781a661f2d9c832a53642bf9d07f51
Tónga <gastongracis@gmail.com>
2024-05-08 21:38:13 -0300

    retoque counter

--- app/producto/[slug]/page.jsx
+++ app/producto/[slug]/page.jsx
@@ -1,5 +1,27 @@
 import ProductDetail from "@/components/Product/ProductDetail";
 
+export const generateMetadata = async ({ params }) => {
+  const { slug } = params;
+
+  const item = await fetch(`${process.env.API_URL}/api/product/${slug}`, {
+    cache: "no-store",
+  }).then((res) => res.json());
+
+  return {
+    title: "[CHOPIN] " + item.title,
+    description: item.description,
+    openGraph: {
+      images: [
+        {
+          url: item.image,
+          width: 1000,
+          height: 1000,
+        },
+      ],
+    },
+  };
+};
+
 const Producto = ({ params }) => {
   const { slug } = params;
   return (
--- components/UI/CounterMini.jsx
+++ components/UI/CounterMini.jsx
@@ -5,21 +5,67 @@ const CounterMini = ({ counter, setCounter, max }) => {
   const increase = () => counter < max && setCounter(counter + 1);
   const decrease = () => counter > 1 && setCounter(counter - 1);
 
+  const handleChange = (e) => {
+    const newValue = parseInt(e.target.value);
+    if (newValue >= 1 && newValue <= max) {
+      setCounter(newValue);
+    }
+  };
+
   return (
-    <div className="flex items-center gap-3">
-      <div
-        onClick={decrease}
-        className="px-2 py-0 rounded  bg-gray-500 hover:bg-gray-600 text-sm cursor-pointer"
-      >
-        -
+    <div className="counter-mini bg-black rounded flex items-center gap-2 p-2">
+      <div className="inline-flex items-center gap-1">
+        <button
+          type="button"
+          onClick={decrease}
+          className="size-6 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-gray-500 bg-gray-800 text-white hover:bg-gray-700 disabled:opacity-50 disabled:pointer-events-none"
+        >
+          <svg
+            className="flex-shrink-0 size-3.5"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M5 12h14"></path>
+          </svg>
+        </button>
+        <input
+          type="number"
+          value={counter}
+          onChange={handleChange}
+          min="1"
+          max={max}
+          className="p-0 w-12 bg-transparent border-0 text-white text-center focus:ring-0 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+        />
+        <button
+          type="button"
+          onClick={increase}
+          className="size-6 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-gray-500 bg-gray-800 text-white hover:bg-gray-700 disabled:opacity-50 disabled:pointer-events-none"
+        >
+          <svg
+            className="flex-shrink-0 size-3.5"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M5 12h14"></path>
+            <path d="M12 5v14"></path>
+          </svg>
+        </button>
       </div>
-      <p className="text-white text-sm">{counter}</p>
-      <div
-        onClick={increase}
-        className="px-2 py-0 rounded  bg-gray-500 hover:bg-gray-600 text-sm cursor-pointer"
-      >
-        +
-      </div>
+      <span className="text-white">Unidades:</span>
     </div>
   );
 };
